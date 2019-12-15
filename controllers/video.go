919f0f0436c729f96062e0d671fedab4da2c325c
lyekumchew <lyekumchew@gmail.com>
2019-12-15 20:13:22 +0800

    refactoring video control logic

--- controllers/video.go
+++ controllers/video.go
@@ -20,43 +20,30 @@ type Video struct {
 }
 
 func (v *Video) Index(c *gin.Context) {
-	var videos []models.Video
-
-	database.DB.Select("id, name, username, created_at, updated_at").Order("id").Find(&videos)
-
-	v.JsonSuccess(c, http.StatusOK, gin.H{"data": videos})
-}
-
-func (v *Video) Show(c *gin.Context) {
 	var video models.Video
+
+	var needPasswd bool
 
 	if database.DB.Where(&models.Video{VideoId: c.Param("video_id")}).First(&video).Error != nil {
 		v.JsonFail(c, http.StatusNotFound, "Data does not exist.")
 		return
 	}
 
-	// need password
-	if video.Password.Valid != false {
-		v.JsonFail(c, http.StatusUnauthorized, "Please provide the password")
-		return
-	}
-
-	// Not Uploaded
+	// if not uploaded yet
 	if video.IsUploaded.Bool == false {
-		v.JsonSuccess(c, http.StatusOK, gin.H{"is_uploaded": 0})
+		v.JsonSuccess(c, http.StatusOK, gin.H{"is_uploaded": false})
 		return
 	}
 
-	url, err := minio.GetObjectUrl(video.ObjectName)
-	if err != nil {
-		log.Println("Failed to get object url")
-		return
+	// if need password
+	if video.Password.Valid == true {
+		needPasswd = true
 	}
 
-	v.JsonSuccess(c, http.StatusOK, gin.H{"url": url})
+	v.JsonSuccess(c, http.StatusOK, gin.H{"video_name": "video_name", "is_uploaded": true, "need_passwd": needPasswd})
 }
 
-func (v *Video) ShowWithPassword(c *gin.Context) {
+func (v *Video) Show(c *gin.Context) {
 	var video models.Video
 	var request QueryRequest
 
@@ -68,56 +55,80 @@ func (v *Video) ShowWithPassword(c *gin.Context) {
 
 		// Not Uploaded
 		if video.IsUploaded.Bool == false {
-			v.JsonSuccess(c, http.StatusOK, gin.H{"is_uploaded": 0})
+			v.JsonFail(c, http.StatusNotFound, "This video is not uploaded yet.")
+			return
+		}
+
+		// if not need to provide the password
+		if video.Password.Valid == false {
+			url, err := minio.GetObjectUrl(video.ObjectName)
+
+			if err != nil {
+				log.Println("Failed to get object url")
+				v.JsonFail(c, http.StatusInternalServerError, "Unable to get object url.")
+				return
+			}
+
+			v.JsonSuccess(c, http.StatusOK, gin.H{"video_name": "video_name", "url": url})
+
 			return
 		}
 
 		if err = bcrypt.CompareHashAndPassword([]byte(video.Password.String), []byte(request.Password)); err != nil {
-			v.JsonFail(c, http.StatusUnauthorized, "Password is wrong")
+			v.JsonFail(c, http.StatusUnauthorized, "Password is wrong.")
 			return
 		}
 
 		url, err := minio.GetObjectUrl(video.ObjectName)
 		if err != nil {
 			log.Println("Failed to get object url")
-			v.JsonFail(c, http.StatusInternalServerError, "Unable to get object url")
+			v.JsonFail(c, http.StatusInternalServerError, "Unable to get object url.")
 			return
 		}
 
-		v.JsonSuccess(c, http.StatusOK, gin.H{"url": url})
+		v.JsonSuccess(c, http.StatusOK, gin.H{"video_name": "video_name", "url": url})
+
+	} else {
+		v.JsonFail(c, http.StatusBadRequest, "Please check your json is ok.")
 	}
 }
 
 func (v *Video) Store(c *gin.Context) {
 	var request CreateRequest
+
 	if err := c.ShouldBind(&request); err == nil {
+		if request.VideoSuffix != "mp4" {
+			v.JsonFail(c, http.StatusBadRequest, "This video format does not allow uploads")
+			return
+		}
+
 		timeLocal, _ := time.LoadLocation("Asia/Shanghai")
 		time.Local = timeLocal
 		t := time.Now().Local()
 		date := fmt.Sprintf("%4d/%2d/%2d/", t.Year(), t.Month(), t.Day())
 
 		videoId := xid.New().String()
-		url, fromData, err := minio.CreatePostUrl(date + videoId)
+		url, formData, err := minio.CreatePostUrl(date + videoId + "." + request.VideoSuffix)
 		if err != nil {
 			v.JsonFail(c, http.StatusBadRequest, err.Error())
 			return
 		}
 
-		var pw sql.NullString
+		var password sql.NullString
 		if request.Password != "" {
 			hash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
 			if err != nil {
 				log.Println(err)
 			}
-			pw = sql.NullString{String: string(hash), Valid: true}
+			password = sql.NullString{String: string(hash), Valid: true}
 		} else {
-			pw = sql.NullString{Valid: false}
+			password = sql.NullString{Valid: false}
 		}
 
 		video := models.Video{
 			VideoId:    videoId,
-			ObjectName: date + videoId,
-			Password:   pw,
+			ObjectName: date + videoId + "." + request.VideoSuffix,
+			Password:   password,
 			DeleteId:   ksuid.New().String(),
 		}
 
@@ -126,7 +137,16 @@ func (v *Video) Store(c *gin.Context) {
 			return
 		}
 
-		v.JsonSuccess(c, http.StatusCreated, gin.H{"url": url, "fromData": fromData})
+		// Test for curl
+		fmt.Printf("curl ")
+		for k, v := range formData {
+			fmt.Printf("-F %s=%s ", k, v)
+		}
+		fmt.Printf("-F file=@test.jpg ")
+		fmt.Printf("%s\n", url)
+
+		v.JsonSuccess(c, http.StatusCreated, gin.H{"upload_url": url, "formData": formData, "video_id": video.VideoId, "delete_id": video.DeleteId})
+
 	} else {
 		v.JsonFail(c, http.StatusBadRequest, err.Error())
 	}
@@ -148,18 +168,28 @@ func (v *Video) Destroy(c *gin.Context) {
 	v.JsonSuccess(c, http.StatusCreated, gin.H{})
 }
 
-func (v *Video) Uploaded(video *models.Video) {
-	if video.ObjectName == "" {
-		log.Println("Can't not found this object")
+// when the file is uploaded, this function will work
+func (v *Video) Uploaded(objectName string) error {
+	var video models.Video
+
+	if objectName == "" {
+		log.Println("Can't not found the object")
 	}
 
-	database.DB.Model(video).Where(video).Update("is_uploaded", sql.NullBool{Valid: true, Bool: true})
+	if err := database.DB.Where("object_name = ?", objectName).First(&video).Error; err != nil {
+		log.Printf("Could not found object_name %v.\n", objectName)
+		return err
+	} else {
+		database.DB.Model(&video).Where(video).Update("is_uploaded", sql.NullBool{Valid: true, Bool: true})
+		return nil
+	}
 }
 
 type CreateRequest struct {
-	Password string `form:"password" json:"password" binding:"max=64"`
+	Password    string `form:"password" json:"password" binding:"max=64"`
+	VideoSuffix string `form:"format" json:"format" binding:"required,min=3,max=4"`
 }
 
 type QueryRequest struct {
-	Password string `form:"password" json:"password" binding:"required,min=1,max=64"`
+	Password string `form:"password" json:"password" binding:"max=64"` // discard "required,min=1,"
 }
--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.13
 require (
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/jinzhu/gorm v1.9.11
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/mattn/go-sqlite3 v2.0.0+incompatible // indirect
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
 	github.com/minio/minio-go/v6 v6.0.42
 	github.com/nsqio/go-nsq v1.0.7
 	github.com/rs/xid v1.2.1
