afbccb82e799da9465ef74d96ab8db398fd983c8
lyekumchew <lyekumchew@gmail.com>
2019-12-15 19:56:37 +0800

    allows all cors origins & update the route

--- routes/router.go
+++ routes/router.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"svf-project/controllers"
 )
@@ -11,11 +12,14 @@ func Init() *gin.Engine {
 	v1 := router.Group("/api/v1")
 	{
 		video := new(controllers.Video)
-		v1.GET("/videos/:video_id", video.Show)
-		v1.POST("/videos/:video_id", video.ShowWithPassword)
+		v1.GET("/videos/:video_id", video.Index)
+		v1.POST("/videos/:video_id", video.Show)
 		v1.POST("/upload", video.Store)
-		v1.GET("/delete/:delete_id", video.Destroy)
+		v1.POST("/delete/:delete_id", video.Destroy)
 	}
+
+	// Default() allows all origins
+	router.Use(cors.Default())
 
 	return router
 }
